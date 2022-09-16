8dd40d57cbbe219c0dd2ab28e9dfa2ce1da3455e
Petebran3 <102988657+Petebran3@users.noreply.github.com>
2022-09-16 19:04:37 -0400

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Component } from 'react'
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-function Main() {
-  return (
-    <div>Main</div>
-  )
+
+
+class Restaurants extends Component {
+  render() {
+    return (
+    
+   <>
+      <Map
+        google ={this.props.google}
+        zoom ={14}
+        style ={{width:"100%", height:"100%"}}
+        initialCenter={
+          {
+            lat: 35.227085,
+            lng: -80.843124
+          }
+        }
+      />
+      </>
+
+    );
+  }
 }
 
-export default Main
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAM49GedQ8pX1gXbsGfu50hz1AphIFGK5E"
+})(Restaurants);
+  
+
+
+
+
